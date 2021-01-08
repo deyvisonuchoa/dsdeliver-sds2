88f762cf120205e608097b44712b163cf1e04ec4
deyvison uchoa <deyvisonuchoa@hotmail.com>
2021-01-08 14:33:37 -0300

    Home page created

--- front-web/src/App.tsx
+++ front-web/src/App.tsx
@@ -1,10 +1,12 @@
 import './App.css';
+import Home from './home';
 import Navbar from './navbar';
 
 function App() {
   return (
     <div className="App">
       <Navbar />
+      <Home />
     </div>
   );
 }
