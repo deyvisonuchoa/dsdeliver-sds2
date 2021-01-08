568456ab2ed788098c35a25ccdab8d8b26b792c8
deyvison uchoa <deyvisonuchoa@hotmail.com>
2021-01-08 11:01:32 -0300

    create front-web initial files

---
+++ front-web/src/App.tsx
@@ -0,0 +1,13 @@
+import './App.css';
+import Counter from './counter';
+import Hello from './Hello';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Hello</h1>
+    </div>
+  );
+}
+
+export default App;
